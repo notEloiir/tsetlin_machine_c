845389354f00763552dfefa332deed2bceeaa6ba
Robert Mesek <rmesek@student.agh.edu.pl>
2025-11-12 21:04:10 +0100

    WIP: add flatbuffers for Python

---
+++ src/tsetlin_machine_py/__init__.py
@@ -0,0 +1,13 @@
+from .c_tsetlin_clf import CTsetlinClassifier
+from .green_tsetlin_clf import GreenTsetlinClassifier
+from .green_tsetlin_sparse_clf import GreenTsetlinSparseClassifier
+from .gt_to_bin import save_to_bin
+from .gt_to_fbs import save_to_fbs
+
+__all__ = [
+    "CTsetlinClassifier",
+    "GreenTsetlinClassifier",
+    "GreenTsetlinSparseClassifier",
+    "save_to_bin",
+    "save_to_fbs",
+]
